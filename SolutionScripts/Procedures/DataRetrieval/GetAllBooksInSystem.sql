bfb00aab594ab05fd9bc6d1fcea78602a6c44ffb
poisonphang <poisonphang@gmail.com>
2020-11-13 11:54:48 -0600

    Added BookCopy and AddBookCopy

--- SolutionScripts/Procedures/DataRetrieval/GetAllBooksInSystem.sql
+++ SolutionScripts/Procedures/DataRetrieval/GetAllBooksInSystem.sql
@@ -2,5 +2,5 @@ CREATE OR ALTER PROCEDURE T7Library.GetAllBooksInSystem
 AS
 SELECT B.Title, BA.FirstName + N' ' + BA.LastName AS Author, B.Year, B.ISBN, B.Publisher, GC.Genre
 FROM T7Library.Book B
-INNER JOIN BookAuthor BA ON B.ISBN = BA.ISBN
-INNER JOIN GenreCode GC On B.GenreCode = GC.Code
+INNER JOIN T7Library.BookAuthor BA ON B.ISBN = BA.ISBN
+INNER JOIN T7Library.GenreCode GC On B.GenreCode = GC.Code;
---
+++ Team7_LibraryApp/Data/BookCopy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team7_LibraryApp.Data
+{
+    public class BookCopy
+    {
+        public int BookId { get; }
+        public string ISBN { get; }
+        public int LocationId { get; }
+        public bool IsCheckedOut { get; }
+        public bool IsOutOfService { get; }
+
+        public BookCopy(int BookId, string ISBN, int LocationId, bool IsCheckedOut, bool IsOutOfService)
+        {
+            this.BookId = BookId;
+            this.ISBN = ISBN;
+            this.LocationId = LocationId;
+            this.IsCheckedOut = IsCheckedOut;
+            this.IsOutOfService = IsOutOfService;
+        }
+    }
+}
