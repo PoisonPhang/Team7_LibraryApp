15748c765e356be01693d3310f162fba76d36542
Nick <nickolas.appino@gmail.com>
2020-11-13 21:48:46 -0600

    Updated

---
+++ Team7_LibraryApp/ReturnConfirmation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Team7_LibraryApp
+{
+    /// <summary>
+    /// Interaction logic for ReturnConfirmation.xaml
+    /// </summary>
+    public partial class ReturnConfirmation : UserControl
+    {
+        public ReturnConfirmation()
+        {
+            InitializeComponent();
+        }
+    }
+}
