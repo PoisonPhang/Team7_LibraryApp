bc0481c944157733413886ff7a2089e1cf9f2a61
poisonphang <poisonphang@gmail.com>
2020-11-17 16:58:21 -0600

    Ranks

--- SolutionScripts/Procedures/DataRetrieval/RankBooks.sql
+++ SolutionScripts/Procedures/DataRetrieval/RankBooks.sql
@@ -4,19 +4,19 @@ AS
 
 IF @GenreCode = 1
 BEGIN
-SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre, RANK() OVER(ORDER BY COUNT(DISTINCT C.CheckoutId)) AS [Rank]
+SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre,DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC) AS [Rank]
 FROM T7Library.Book B
 INNER JOIN T7Library.BookAuthor BA ON B.ISBN = BA.ISBN
 INNER JOIN T7Library.GenreCode GC ON B.GenreCode = GC.Code
 INNER JOIN T7Library.BookCopy BC ON B.ISBN = BC.ISBN
 INNER JOIN T7Library.Checkout C ON BC.BookId = C.BookId
 GROUP BY B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName, GC.Genre
-ORDER BY RANK() OVER(ORDER BY COUNT(DISTINCT C.CheckoutId));
+ORDER BY DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC);
 END;
 
 IF @GenreCode <> 1
 BEGIN
-SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre, RANK() OVER(ORDER BY COUNT(DISTINCT C.CheckoutId)) AS [RANK]
+SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre, DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC) AS [RANK]
 FROM T7Library.Book B
 INNER JOIN T7Library.BookAuthor BA ON B.ISBN = BA.ISBN
 INNER JOIN T7Library.GenreCode GC ON B.GenreCode = GC.Code
@@ -24,5 +24,5 @@ INNER JOIN T7Library.BookCopy BC ON B.ISBN = BC.ISBN
 INNER JOIN T7Library.Checkout C ON BC.BookId = C.BookId
 WHERE GC.Code = @GenreCode
 GROUP BY B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName, GC.Genre
-ORDER BY RANK() OVER(ORDER BY COUNT(DISTINCT C.CheckoutId));
+ORDER BY DENSE_RANK() OVER(PARTITION BY GC.Genre ORDER BY COUNT(C.CheckoutId) DESC) ASC;
 END;
