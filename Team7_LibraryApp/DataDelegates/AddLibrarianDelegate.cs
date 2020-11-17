b23a252275fe8571a6fbe0f09ffb816b12b341a9
poisonphang <poisonphang@gmail.com>
2020-11-17 17:04:19 -0600

    Passwordhashing

--- Team7_LibraryApp/DataDelegates/AddLibrarianDelegate.cs
+++ Team7_LibraryApp/DataDelegates/AddLibrarianDelegate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess;
@@ -17,10 +18,10 @@ namespace Team7_LibraryApp.DataDelegates
         public readonly string LastName;
         public readonly string StartDate;
 
-        public AddLibrarianDelegate(string Username, string passwordHash, string FirstName, string LastName, string StartDate) : base("T7Library.AddLibrarian")
+        public AddLibrarianDelegate(string Username, string password, string FirstName, string LastName, string StartDate) : base("T7Library.AddLibrarian")
         {
             this.Username = Username;
-            this.PasswordHash = passwordHash;
+            this.PasswordHash = HashPassword(password);
             this.FirstName = FirstName;
             this.LastName = LastName;
             this.StartDate = StartDate;
@@ -35,6 +36,27 @@ namespace Team7_LibraryApp.DataDelegates
             command.Parameters.AddWithValue("FirstName", FirstName);
             command.Parameters.AddWithValue("LastName", LastName);
             command.Parameters.AddWithValue("StartDate", StartDate);
+        }
+
+        public string HashPassword(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text  
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
+
+            //get hash result after compute it  
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits  
+                //for each byte  
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
         }
     }
 }
