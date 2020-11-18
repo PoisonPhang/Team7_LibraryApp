0f96231558110548b1664bd3383c3919be2b9667
poisonphang <poisonphang@gmail.com>
2020-11-18 10:15:12 -0600

    yeet

--- SolutionScripts/Procedures/DataRetrieval/RankBooks.sql
+++ SolutionScripts/Procedures/DataRetrieval/RankBooks.sql
@@ -4,7 +4,7 @@ AS
 
 IF @GenreCode = 1
 BEGIN
-SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre,DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC) AS [Rank]
+SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre,DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC) AS [Rank], COUNT(C.CheckoutId)
 FROM T7Library.Book B
 INNER JOIN T7Library.BookAuthor BA ON B.ISBN = BA.ISBN
 INNER JOIN T7Library.GenreCode GC ON B.GenreCode = GC.Code
@@ -16,7 +16,7 @@ END;
 
 IF @GenreCode <> 1
 BEGIN
-SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre, DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) DESC) AS [RANK]
+SELECT B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName AS BookAuthor, GC.Genre, DENSE_RANK() OVER(ORDER BY COUNT(C.CheckoutId) ASC) AS [RANK]
 FROM T7Library.Book B
 INNER JOIN T7Library.BookAuthor BA ON B.ISBN = BA.ISBN
 INNER JOIN T7Library.GenreCode GC ON B.GenreCode = GC.Code
@@ -24,5 +24,5 @@ INNER JOIN T7Library.BookCopy BC ON B.ISBN = BC.ISBN
 INNER JOIN T7Library.Checkout C ON BC.BookId = C.BookId
 WHERE GC.Code = @GenreCode
 GROUP BY B.ISBN, B.Title, BA.FirstName + ' ' + BA.LastName, GC.Genre
-ORDER BY DENSE_RANK() OVER(PARTITION BY GC.Genre ORDER BY COUNT(C.CheckoutId) DESC) ASC;
+ORDER BY DENSE_RANK() OVER(PARTITION BY GC.Genre ORDER BY COUNT(C.CheckoutId) DESC);
 END;
